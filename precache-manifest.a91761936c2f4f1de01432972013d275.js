29264df67915898db44b0fe433892a43d2545b7f
TamirAtia <tamir111994@gmail.com>
2020-07-29 15:13:51 +0300

    Updates

---
+++ precache-manifest.a91761936c2f4f1de01432972013d275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25895b738eecb1de3ef90f200b7fcc68",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a8e37a473f447959546b",
+    "url": "/monsters-rolodex/static/css/main.b75d7000.chunk.css"
+  },
+  {
+    "revision": "5db257ba903a8fee2e3c",
+    "url": "/monsters-rolodex/static/js/2.c75ba697.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.c75ba697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8e37a473f447959546b",
+    "url": "/monsters-rolodex/static/js/main.f0fbeb1b.chunk.js"
+  },
+  {
+    "revision": "0bd73d68cf04fc8e9a1d",
+    "url": "/monsters-rolodex/static/js/runtime-main.b775fccc.js"
+  }
+]);
